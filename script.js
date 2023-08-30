b3f443a99c35c8a3e2a6f4d196867287d3a86bbd
ytaj <138846103+ytaj@users.noreply.github.com>
2023-08-30 10:05:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-// 見る人の心に寄り添い、時間の経過を優雅に伝えるコード
+// 心静かに、時間の流れを可視化するアナログ時計のためのコード
 function updateClock() {
     const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
     
-    const hoursSpan = document.getElementById('hours');
-    const minutesSpan = document.getElementById('minutes');
-    const secondsSpan = document.getElementById('seconds');
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
     
-    hoursSpan.textContent = hours;
-    minutesSpan.textContent = minutes;
-    secondsSpan.textContent = seconds;
+    const hourHand = document.getElementById('hour-hand');
+    const minuteHand = document.getElementById('minute-hand');
+    const secondHand = document.getElementById('second-hand');
+    
+    const hourDeg = (hours % 12) * 30 + (minutes / 60) * 30;
+    const minuteDeg = minutes * 6 + (seconds / 60) * 6;
+    const secondDeg = seconds * 6;
+    
+    hourHand.style.transform = `rotate(${hourDeg}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+    secondHand.style.transform = `rotate(${secondDeg}deg)`;
 }
 
-// 時間の経過を感じる心、その流れに合わせてコードも流れる
-updateClock(); // 最初の表示を即座に行う
+// 心の奥深く、時間の響きに耳を傾けつつ、美しいコードが描かれる
+updateClock(); // 初めての描画を行う
 setInterval(updateClock, 1000); // 1秒ごとに更新
